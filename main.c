a93ddcd74b0a9e800ffd59bd84ceef7e5a3fc631
u238 <urancore.git@gmail.com>
2025-08-06 19:34:16 +0700

    print work time

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "cli.h"
 #include "emulator.h"
@@ -19,16 +20,22 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	clock_t start, end;
 
 	parse_labels(&emu);
 	load_program(&emu);
 	fclose(emu.fp);
+
+	start = clock();
 
 	while (emu.pc < emu.total_lines) {
 		if (!exec_line(&emu, emu.lines[emu.pc])) break;
 		if (emu.debug) debug_message(&emu);
 		emu.pc++;
 	}
+	end = clock();
+	double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("work time %.6fs\n", cpu_time_used);
 
 	for (int i = 0; i < emu.total_lines; i++) {
 		free(emu.lines[i]);
